a78949fb4698046a0887fa5abbed0b5fbfc8152a
Nava-151 <n87891@gmail.com>
2025-02-06 21:44:28 +0200

    node json changed

--- renderProjects/app.js
+++ renderProjects/app.js
@@ -1,27 +1,24 @@
-const express = require("express");
-const axios = require("axios");
+
+import express from 'express';
+import renderApi from '@api/render-api';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const RENDER_API_URL = "https://api.render.com/v1/services";
-const API_KEY = 'rnd_SsQxCHohB8tkk4uyy0PzVZi6p6TQ';
+// אימות מול Render API
+renderApi.auth('rnd_qJEXf7yqzg68uH3oC4v6nCz1kkjM');
 
-app.get("/", async (req, res) => {
+app.get('/', async (req, res) => {
   try {
-    const response = await axios.get(RENDER_API_URL, {
-      headers: {
-        Authorization: `Bearer ${API_KEY}`,
-      },
-    });
-
-    res.json(response.data);
-  } catch (error) {
-    console.error("Error fetching data from Render API:", error);
-    res.status(500).json({ error: "Failed to fetch data from Render API" });
+    const { data } = await renderApi.listServices({ includePreviews: 'true', limit: '20' });
+    res.json(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error fetching data from Render API');
   }
 });
 
+// הפעלת השרת
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
